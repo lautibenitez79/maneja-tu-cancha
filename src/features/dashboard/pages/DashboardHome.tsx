f204545ff049ff2d671a5081475036c4cfa94371
lautibenitez79 <lauti.benitez.nahuel@gmail.com>
2026-09-04 16:29:02 -0300

    complete user roles and permissions

--- src/features/dashboard/pages/DashboardHome.tsx
+++ src/features/dashboard/pages/DashboardHome.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
 
 import DashboardHeader from "../components/DashboardHeader";
