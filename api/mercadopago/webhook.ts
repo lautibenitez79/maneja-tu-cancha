861dca67fa09c013caa44093e581cab87143eeba
lautibenitez79 <lauti.benitez.nahuel@gmail.com>
2026-09-18 17:19:01 -0300

    debug email cuota mensual

--- api/mercadopago/webhook.ts
+++ api/mercadopago/webhook.ts
@@ -1394,6 +1394,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
         console.log("Datos de email de cuota:", {
           fee_id: fee.id,
+          
           customer_email: fee.customer_email,
           customer_name: fee.customer_name,
         });
